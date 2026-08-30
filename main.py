75bcfb564dc89a322b79bbbb9bc54640ec027835
tejungchou <roger04130413@gmail.com>
2026-08-29 22:33:04 -0700

    Add ECC rule and previous gpu records

--- main.py
+++ main.py
@@ -28,6 +28,12 @@ class FakeGPU:
         # T = 0.5 U + 40
         self.temperature = 0.5 * self.utilization + 40
 
+    def inject_ecc_error(self, error_type):
+        if error_type == 'double':
+            self.ecc_double_bit_errors += 1
+
+        elif error_type == 'single':
+            self.ecc_single_bit_errors += 1
 
 class FakeServer:
     def __init__(self, server_id, rack_id, num_gpus=4):
@@ -66,13 +72,19 @@ class FakeAgent:
 class FakeObserver:
     def __init__(self, rules):
         self.rules = rules
+        self.previous_telemetry = {}
 
     def observe(self, telemetry):
         issues = []
 
         for gpu in telemetry:
+            key = (gpu['server_id'], gpu['gpu_id'])
+
+            previous_gpu = self.previous_telemetry.get(key)
+
             for rule in self.rules:
-                result = rule.check(gpu)
+                result = rule.check(gpu, previous_gpu)
+
                 if result:
                     issues.append({
                         'rack_id': gpu['rack_id'],
@@ -82,11 +94,12 @@ class FakeObserver:
                         'severity': result['severity'],
                         'detect_time': gpu['collect_time']
                     })
+            self.previous_telemetry[key] = gpu
 
         return issues
 
 class TemperatureRule:
-    def check(self, gpu):
+    def check(self, gpu, previous_gpu=None):
 
         temperature = gpu['gpu_temperature']
 
@@ -105,13 +118,43 @@ class TemperatureRule:
                 'severity': 'CRITICAL'
             }
         
+class ECCRule:
+    def check(self, gpu, previous_gpu):
+        if not previous_gpu:
+            prev_ecc_error_single = 0
+            prev_ecc_error_double = 0
+        else:
+            prev_ecc_error_single = previous_gpu['gpu_ecc_single_bit_errors']
+            prev_ecc_error_double = previous_gpu['gpu_ecc_double_bit_errors']
+
+
+        ecc_error_single = gpu['gpu_ecc_single_bit_errors']
+        ecc_error_double = gpu['gpu_ecc_double_bit_errors']
+
+        if ecc_error_double > prev_ecc_error_double:
+            return {
+                'issue_type': 'UNCORRECTABLE_ERROR',
+                'severity': 'CRITICAL'
+            }
+        elif ecc_error_single > prev_ecc_error_single:
+            return {
+                'issue_type': 'CORRECTABLE_ERROR',
+                'severity': 'WARNING'
+            }
+        else:
+            return None
 
 
 def main():
     server = FakeServer('server-1', 'rack-1', 4)
     agent = FakeAgent(server)
     temperature_rule = TemperatureRule()
-    observer = FakeObserver([temperature_rule])
+    ecc_rule = ECCRule()
+    server.gpus[0].inject_ecc_error('single')
+    observer = FakeObserver([
+        temperature_rule,
+        ecc_rule
+    ])
 
     report = agent.collect()
     issues = observer.observe(report)
